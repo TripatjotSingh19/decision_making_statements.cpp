5978f80244235804eb4fbe463039336b6d73d227
TripatjotSingh19 <tripatjots646@gmail.com>
2025-07-27 16:15:43 +0530

    Create vowel checker.cpp

---
+++ vowel checker.cpp
@@ -0,0 +1,24 @@
+//Tripatjot Singh ENTC A2
+//24070123044
+#include<iostream>
+using namespace std;
+int main()
+{
+  char str;
+  cout << "Enter a Character : ";
+  cin >> str;
+  if (str=='a' || str=='e' || str=='i' || str=='o' || str=='u'|| str=='A' || str=='E' || str=='I'||str=='O'|| str=='U')
+  {
+    cout << "The Character entered is a Vowel";
+  }
+  else {
+    cout << "The Character entered is a Consonant";
+  }
+}
+/*
+Output:
+Enter a Character : q
+The Character entered is a Consonant
+Enter a Character : a
+The Character entered is a Vowel
+*/
